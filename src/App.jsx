310743971ac216c81c5a9a035e919559773dbf01
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-14 15:31:58 +0200

    style : finish compaines style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 
 import iconCompany from "./assets/icom-logo.png";
+import smartLogo from "./assets/Smart-Logo.png";
+import westenOffice from "./assets/westin-guest-office-logo.png";
+import usvSystem from "./assets/computerSystem.svg";
+import media from "./assets/Media_24.png";
 
 function App() {
   return (
@@ -13,22 +17,29 @@ function App() {
   );
 }
 
-function Companies(){
-  return(
+function Companies() {
+  return (
     <div className="compaines section-padding d-flex justify-content-between align-items-center">
-      <p className="paragraph">Compaines we have build</p>
-      <ul className="compaines__list d-flex">
-        <li className="company__list-item"></li>
-        <li className="company__list-item"></li>
-        <li className="company__list-item"></li>
-        <li className="company__list-item"></li>
+      <p className="paragraph fs-3">Compaines we have build</p>
+      <ul className="compaines__list d-flex  align-items-center  ">
+        <li className="company__list-item mt-3">
+          <img src={media} alt="company" />
+        </li>
+        <li className="company__list-item mt-3">
+          <img src={usvSystem} alt="company" />
+        </li>
+        <li className="company__list-item mt-3">
+          <img src={westenOffice} alt="company" />
+        </li>
+        <li className="company__list-item mt-3">
+          <img src={smartLogo} alt="company" />
+        </li>
         <li className="company__list-item">
           <img src={iconCompany} alt="company" />
         </li>
       </ul>
     </div>
-  )
+  );
 }
-
 
 export default App;
