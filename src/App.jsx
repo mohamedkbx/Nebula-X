66b8f87ed5c90ce4dc1704666687f34803eb43cd
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-14 05:31:23 +0200

    feat: finish hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import Header from "./components/Header";
 
+import hero from "./assets/hero-1.webp";
+import profileCard from "./assets/Layer 6.webp";
+import backFace from "./assets/Shape 1.webp";
 
 function App() {
   return (
-    <div >
+    <div>
+     
       <Header />
       <HeroSection />
     </div>
   );
 }
 
-function HeroSection(){
-  return(
+function HeroSection() {
+  return (
     <section className="hero section-padding">
-      
+       <img src={backFace} alt="back"  className="backFace"/>
+      <div className="hero__content">
+        <div className="hero-img">
+          <img src={hero} alt="hero-img" />
+          <div className="hero-img__overlay">
+            <img src={profileCard} alt="profile Card" />
+            <div className="hero-img__text">
+              <h3>James Prono</h3>
+              <p>Digital Marketer</p>
+            </div>
+          </div>
+        </div>
+        <div className="hero-text">
+          <h1 className="primary-header">
+            We Help You Build & Grow an Online Business
+          </h1>
+          <div className="search-input">
+          <input type="text" placeholder="Search for a course" />
+          <button className="custom-btn">Search</button>
+        </div>
+        </div>
+        
+      </div>
     </section>
-  )
+  );
 }
 export default App;
