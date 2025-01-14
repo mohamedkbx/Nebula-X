d8d98f31556dad5fdac5e4bc30b1778603fe312e
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-14 16:43:28 +0200

    feat: product component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,24 @@ function App() {
       <Header />
       <HeroSection />
       <Companies />
+      <ProductSection />
     </div>
+  );
+}
+
+function ProductSection(){
+  return(
+    <section className="product section-padding ">
+      <div className="product__text ">
+        <span>WE HELP YOU</span>
+        <h2>Sell Stunnig Products</h2>
+        <p>We&apos;ll guide you through our unique 5-step brand-building framework</p>
+      </div>
+      <div className="product__items">
+        <div className="product__item-1"></div>
+        <div className="product__item-2"></div>
+      </div>
+    </section>
   );
 }
 
