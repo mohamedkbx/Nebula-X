b3dd5d52f47f43506ccd10042d002b2f2b88b51d
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-15 23:50:32 +0200

    fix: fix grid system in hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,36 +1,38 @@
-
 import hero from "../assets/hero-1.webp";
 import profileCard from "../assets/Layer 6.webp";
 import backFace from "../assets/Shape 1.webp";
 import { FaSistrix } from "react-icons/fa6";
 
-export default  function HeroSection() {
-    return (
-      <section className="hero section-padding">
-        <img src={backFace} alt="back" className="backFace" />
-        <div className="hero__content">
-          <div className="hero-img">
-            <img src={hero} alt="hero-img" />
-            <div className="hero-img__overlay">
-              <img src={profileCard} alt="profile Card" />
-              <div className="hero-img__text">
-                <h3>James Prono</h3>
-                <p>Digital Marketer</p>
-              </div>
-            </div>
-          </div>
-          <div className="hero-text">
-            <h1 className="primary-header">
-              We Help You Build & Grow an Online Business
-            </h1>
-            <div className="search-input">
-              <input type="text" placeholder="Tell us what you need to build..." />
-              <button className="search-btn">
-                <FaSistrix />
-              </button>
+export default function HeroSection() {
+  return (
+    <section className="hero ">
+      {/* <img src={backFace} alt="back" className="backFace" /> */}
+      <div className="hero__content row section-padding justify-content-around">
+        <div className="hero-img col-12 col-lg-6">
+          <img src={hero} alt="hero-img" className="image-fluid " />
+          <div className="hero-img__overlay">
+            <img src={profileCard} alt="profile Card"className="image-fluid "/>
+            <div className="hero-img__text">
+              <h3>James Prono</h3>
+              <p>Digital Marketer</p>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
+        <div className="hero-text col-12 col-md-6">
+          <h1 className="primary-header">
+            We Help You Build & Grow an Online Business
+          </h1>
+          <div className="search-input">
+            <input
+              type="text"
+              placeholder="Tell us what you need to build..."
+            />
+            <button className="search-btn">
+              <FaSistrix />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
