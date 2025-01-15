d4120a636881b907f53c8add6c4f906da0e7d544
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-15 15:59:31 +0200

    feat: setup contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import ProductSection from "./components/ProductSection";
 import Shop from "./components/Shop";
 import Sales from "./components/Sales";
 
+import contactOverlay from "./assets/Shape 2.png";
+import contactImg from "./assets/contactImg.png";
+
 
 function App() {
   return (
@@ -22,8 +25,15 @@ function App() {
 
 function ContactUs() {
   return (
-    <section className="contact section-padding">
-      <div className="contact-content section-padding ">fdsfsf</div>
+    <section className="contact  ">
+          <img src={contactOverlay} alt="overlay" className="image-fluid contact__overlay" />
+        
+      <div className="contact-content section-padding row d-flex justify-content-between">
+        <div className="contact__form col-12 col-md-4 mx-auto">ffsfdsf</div>
+        <div className="contact__img col-12 col-md-4">
+          <img src={contactImg} alt="contact img" />
+        </div>
+      </div>
     </section>
   );
 }
